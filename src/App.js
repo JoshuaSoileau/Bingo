b4e12ffc7b6efbdecf8863bb78aa7180dd3fe799
Joshua Soileau <joshua.soileau@gmail.com>
2020-12-03 09:31:43 -0500

    Fixes the title & wide screen layout

--- src/App.js
+++ src/App.js
@@ -30,50 +30,53 @@ function App() {
         rel="stylesheet"
       />
 
-      <h1 className="px-4 font-serif font-extrabold text-xl sm:text-3xl md:text-5xl lg:text-6xl text-red-600 flex justify-between">
-        <strong className="headline">Hallmark Movie Bingo!</strong>
-        <button
-          type="button"
-          onClick={() => {
-            if (confirm("Are you sure you want to refresh your tiles?"))
-              refresh();
-          }}
-          aria-label="Refresh tiles?"
-        >
-          <span role="img" aria-label="Refresh icon">
-            🔄
-          </span>
-        </button>
-      </h1>
+      <div className="max-w-4xl mx-auto">
+        <h1 className="px-4 font-serif font-extrabold text-5xl lg:text-6xl text-red-600 flex flex-col md:flex-row justify-between items-start mb-12">
+          <strong className="headline">Hallmark Movie Bingo!</strong>
+          <button
+            className="block my-6 md:my-0 order-first md:order-last"
+            type="button"
+            onClick={() => {
+              if (confirm("Are you sure you want to refresh your tiles?"))
+                refresh();
+            }}
+            aria-label="Refresh tiles?"
+          >
+            <span role="img" aria-label="Refresh icon">
+              🔄
+            </span>
+          </button>
+        </h1>
 
-      <ul className="grid grid-cols-5 grid-rows-5 gap-0">
-        {Object.entries(state).map(([key, value], index) => {
-          const row = Math.floor(index / 5);
-          const column = index % 5;
+        <ul className="grid grid-cols-5 grid-rows-5 gap-0">
+          {Object.entries(state).map(([key, value], index) => {
+            const row = Math.floor(index / 5);
+            const column = index % 5;
 
-          const buttonClassName = classnames(
-            "h-full flex items-center text-left w-full",
-            "text-xs md:text-sm lg:text-normal",
-            "p-2 sm:p-4 md:py-8 lg:py-12",
-            "focus:outline-none",
-            !value && "bg-gray-100",
-            value && "bg-gray-800 text-white",
-            getColorClass(row, column, value)
-          );
+            const buttonClassName = classnames(
+              "h-full flex items-center text-left w-full",
+              "text-xs md:text-sm lg:text-normal",
+              "p-2 sm:p-4 md:py-8 lg:py-12",
+              "focus:outline-none",
+              !value && "bg-gray-100",
+              value && "bg-gray-800 text-white",
+              getColorClass(row, column, value)
+            );
 
-          return (
-            <li key={key}>
-              <button
-                type="button"
-                className={buttonClassName}
-                onClick={() => toggle(key)}
-              >
-                {key}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+            return (
+              <li key={key}>
+                <button
+                  type="button"
+                  className={buttonClassName}
+                  onClick={() => toggle(key)}
+                >
+                  {key}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
 
       <Snow />
     </div>
